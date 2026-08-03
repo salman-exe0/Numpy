c4fe2763c28c55ef5a38c228406996685ccf5445
salman.exe0 <sunnylurka0@gmal.com>
2026-08-03 15:34:31 +0500

    Initial commit

---
+++ firstnumpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class FirstNumpy:
+    def __init__(self):
+        self.myvl=np.array([1, 2, 3, 4, 5])
+        print("My array is:", self.myvl)
+        print(type(self.myvl))
+
+np1 = FirstNumpy()
