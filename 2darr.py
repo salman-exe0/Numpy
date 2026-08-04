6df62cc201f9b30c224e4f386f41b670af978404
salman.exe0 <sunnylurka0@gmal.com>
2026-08-04 13:21:00 +0500

    Refactor 2D array printing and add new example for arange function

--- 2darr.py
+++ 2darr.py
@@ -1,5 +1,5 @@
 import numpy as np
 rr=np.array([[1, 2, 3], [4, 5, 6]])
-print(rr)
 for i in rr:
-    print(i)
+    for j in i:
+        print(j)
---
+++ arrangeInNumpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+arrangenumpy=np.arange(1,20,3)
+print(arrangenumpy)
+for i in arrangenumpy:
+    print(i)
